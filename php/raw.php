3d593edf358f3e23d8fa5c2869e86eee7a850e45
fredrikstromlarsen <07fredrik@gmail.com>
2022-05-28 14:01:09 +0200

    Sanitize JSON to prevent XSS

--- php/raw.php
+++ php/raw.php
@@ -1,8 +1,11 @@
 <?php
-header('Content-type: application/json');
 
 if (isset($_GET["d"])) {
+    header('Content-type: application/json');
     $dataB64 = urldecode($_GET["d"]);
     $dataJSON = base64_decode($dataB64);
+
+    // copilot: Sanitize $dataJSON to prevent XSS
+    $dataJSON = preg_replace('/[^a-zA-Z0-9_,:{}\[\]\.\s\n\r\t-#&;]/', '', $dataJSON);
     echo $dataJSON;
 } else header("Location:../");
